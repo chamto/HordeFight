212d7f8dd8b98bd1eaef49471181cfded86b0850
chamto <anrudco@gmail.com>
2019-02-14 20:07:37 +0900

    [ 구트리 : 소스정리 ]

--- Assets/z_Test/Acceleration_Algorithm/SphereTree/SphereTree.cs
+++ Assets/z_Test/Acceleration_Algorithm/SphereTree/SphereTree.cs
@@ -20,54 +20,6 @@ public class SphereTree : MonoBehaviour
 }
 
 
-public class DefineO
-{
 
-
-    public static int SCREEN_WIDTH = 1024;
-    public static int SCREEN_HEIGHT = 768;
-
-    public static int FIXED = 16;//16;
-    public static int SW_ID = (SCREEN_WIDTH * FIXED); //1024 * 16 = 16384
-    public static int SH_IT = (SCREEN_HEIGHT * FIXED); //768 * 16 = 12288
-
-    public static int G_CENTER_X = SW_ID / 2; //16384 / 2 = 8192
-    public static int G_CENTER_Y = SH_IT / 2; //12288 / 2 = 6144
-
-
-    static public void DrawLine(float x1, float y1, float x2, float y2, uint color)
-    {
-        Color cc = Misc.Color32_ToColor(Misc.Hex_ToColor32(color));
-        //DebugWide.LogBlue(cc);
-        //cc.a = 1f;
-
-
-        Gizmos.color = cc;
-        Gizmos.DrawLine(new Vector3(x1, y1, 0), new Vector3(x2, y2, 0));
-    }
-
-    static public void DrawCircle(float locx, float locy, float radius, uint color)
-    {
-        Color cc = Misc.Color32_ToColor(Misc.Hex_ToColor32(color));
-        //DebugWide.LogBlue(cc + "    " + DefineO.HexToColor(color) + "    " + color);
-        //cc.a = 1f;
-
-        //UnityEditor.Handles.color = Color.red;
-        Gizmos.color = cc;
-        Gizmos.DrawWireSphere(new Vector3(locx, locy, 0), radius);
-    }
-
-    static public void PrintText(float x, float y, uint color, string text)
-    {
-        //UnityEditor.Handles.color = Misc.Color32_ToColor(Misc.Hex_ToColor32(color));
-
-        GUIStyle style = new GUIStyle();
-        style.normal.textColor = Misc.Color32_ToColor(Misc.Hex_ToColor32(color));
-
-        UnityEditor.Handles.BeginGUI();
-        UnityEditor.Handles.Label(new Vector3(x, y, 0), text, style);
-        UnityEditor.Handles.EndGUI();
-    }
-}
 //===================================================
 
--- Assets/z_Test/Acceleration_Algorithm/SphereTree/Test_SphereTree.cs
+++ Assets/z_Test/Acceleration_Algorithm/SphereTree/Test_SphereTree.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UtilGS9;
 
 public class Test_SphereTree : MonoBehaviour 
 {
@@ -13,7 +14,7 @@ public class Test_SphereTree : MonoBehaviour
 	// Use this for initialization
 	void Start () 
     {
-        _circleFactory = new CircleFactory(100);
+        _circleFactory = new CircleFactory(20);
         _circleFactory.SetState(Circle.State.SHOW_ALL); //CS_SHOW_RAYTRACE , CS_SHOW_FRUSTUM , CS_SHOW_RANGE_TEST
 		
 	}
@@ -79,4 +80,53 @@ public class Test_SphereTree : MonoBehaviour
         _circleFactory.Render();
 	}
 
+}
+
+public class DefineO
+{
+
+    public static int SCREEN_WIDTH = 1024;
+    public static int SCREEN_HEIGHT = 768;
+
+    public static int FIXED = 16;//16;
+    public static int SW_ID = (SCREEN_WIDTH * FIXED); //1024 * 16 = 16384
+    public static int SH_IT = (SCREEN_HEIGHT * FIXED); //768 * 16 = 12288
+
+    public static int G_CENTER_X = SW_ID / 2; //16384 / 2 = 8192
+    public static int G_CENTER_Y = SH_IT / 2; //12288 / 2 = 6144
+
+
+    static public void DrawLine(float x1, float y1, float x2, float y2, uint color)
+    {
+        Color cc = Misc.Color32_ToColor(Misc.Hex_ToColor32(color));
+        //DebugWide.LogBlue(cc);
+        //cc.a = 1f;
+
+
+        Gizmos.color = cc;
+        Gizmos.DrawLine(new Vector3(x1, y1, 0), new Vector3(x2, y2, 0));
+    }
+
+    static public void DrawCircle(float locx, float locy, float radius, uint color)
+    {
+        Color cc = Misc.Color32_ToColor(Misc.Hex_ToColor32(color));
+        //DebugWide.LogBlue(cc + "    " + DefineO.HexToColor(color) + "    " + color);
+        //cc.a = 1f;
+
+        //UnityEditor.Handles.color = Color.red;
+        Gizmos.color = cc;
+        Gizmos.DrawWireSphere(new Vector3(locx, locy, 0), radius);
+    }
+
+    static public void PrintText(float x, float y, uint color, string text)
+    {
+        //UnityEditor.Handles.color = Misc.Color32_ToColor(Misc.Hex_ToColor32(color));
+
+        GUIStyle style = new GUIStyle();
+        style.normal.textColor = Misc.Color32_ToColor(Misc.Hex_ToColor32(color));
+
+        UnityEditor.Handles.BeginGUI();
+        UnityEditor.Handles.Label(new Vector3(x, y, 0), text, style);
+        UnityEditor.Handles.EndGUI();
+    }
 }
