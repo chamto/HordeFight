0e613fb0dc528b99b8a92e1f8d4cd43afe47cf4a
chamto <anrudco@gmail.com>
2019-08-10 08:52:58 +0900

    [ 변형원 테스트 프로젝트 추가 : 임의의 조절점으로 원을 늘리는 기능 테스트 ]

---
+++ Assets/z_Test/DeformationCircle/DeformationCircle.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeformationCircle : MonoBehaviour 
+{
+
+    public Transform highestPoint = null;
+    public Transform anchorPointA = null;
+    public Transform anchorPointB = null;
+
+	// Use this for initialization
+	void Start () 
+    {
+        highestPoint = GameObject.Find("highestPoint").transform;
+        anchorPointA = GameObject.Find("anchorPointA").transform;
+        anchorPointB = GameObject.Find("anchorPointB").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+    //**** 1사분면에서만 정상 동작한다 ****
+	private void OnDrawGizmos()
+	{
+        if (null == highestPoint) return;
+
+        float radius = 10f;
+
+        DebugWide.DrawCircle(Vector3.zero, radius, Color.black);
+        DebugWide.DrawLine(Vector3.zero, Vector3.right * radius, Color.gray);
+        DebugWide.DrawLine(Vector3.zero, Vector3.forward * radius, Color.gray);
+
+        DebugWide.DrawLine(anchorPointA.position, highestPoint.position, Color.green);
+        DebugWide.DrawLine(anchorPointB.position, highestPoint.position, Color.green);
+
+
+        //늘어남계수 = 원점에서 최고점까지의 길이 - 반지름 
+        float t = highestPoint.position.magnitude - radius; 
+
+        float angleA = Vector3.SignedAngle(Vector3.forward,anchorPointA.position,  Vector3.up);
+        float angleB = Vector3.SignedAngle(Vector3.forward,anchorPointB.position,   Vector3.up);
+        float angleH = Vector3.SignedAngle(Vector3.forward,highestPoint.position,   Vector3.up);
+        DebugWide.PrintText(anchorPointA.position, Color.black, "A : " + angleA);
+        DebugWide.PrintText(anchorPointB.position, Color.black, "B : " + angleB);
+        DebugWide.PrintText(highestPoint.position, Color.black, "H : " + angleH + "  t : " + t);
+
+        //비례식을 이용하여 td 구하기 
+        //angleD : td  = angleH : t
+        //td * angleH = angleD * t
+        //td = (angleD * t) / angleH
+        float maxAngle = angleA > angleB ? angleA : angleB;
+        float maxTd = (maxAngle * t) / angleH;
+
+        float count = 30;
+        for (int i = 0; i < count;i++)
+        {
+            float angleD = Mathf.LerpAngle(angleA, angleB, i / (float)count);
+            //float angleD = Mathf.Lerp(angleA, angleB, i / (float)count);
+            Vector3 tdPos = Quaternion.AngleAxis(angleD, Vector3.up) * Vector3.forward;
+
+            //if (angleD > angleH) angleD = maxAngle - angleD;
+            float td = (angleD * t) / angleH;
+            //if (td > t) td = maxTd - td; //최고점을 기준으로 대칭형을 만들어 준다 
+            if (td > t) 
+            {
+                //최고점을 기준으로 대칭형을 만들어 준다    
+                td = td - maxTd;
+                td *= -1f;
+                td /= (maxTd - t); //1~0로 변환
+                td *= t; //t~0 범위값으로 바꾸어 준다 
+            }
+
+
+            tdPos = tdPos * (radius + td);
+            DebugWide.DrawLine(Vector3.zero, tdPos, Color.red);
+            //DebugWide.PrintText(tdPos, Color.black, " " + td);
+        }
+	}
+}
