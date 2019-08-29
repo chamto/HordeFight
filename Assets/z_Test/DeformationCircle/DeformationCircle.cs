2e9ba0923dc210a2988d7ab77ab41919c2d48553
chamto <anrudco@gmail.com>
2019-08-30 05:18:48 +0900

    [ 변형원 테스트 프로젝트 :   모든 사분면에서 변형 표현 , 임의의 각에서도 변형 표현 , 보간표현 , 초기방향 기준 -90 , +90도 회전한 방향 출력(앵커A,B의 최대 범위를 나타냄) ,  최고점에서 -90도 회전한 방향을 "초기방향"으로 설정하여 문제를 해결함 ]

--- Assets/z_Test/DeformationCircle/DeformationCircle.cs
+++ Assets/z_Test/DeformationCircle/DeformationCircle.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class DeformationCircle : MonoBehaviour 
 {
 
+    public bool isTornado = true;
+    public bool isInter = false;
+
     public float _radius = 10f;
     public Transform sphereCenter = null;
     public Transform highestPoint = null;
@@ -28,23 +31,12 @@ public class DeformationCircle : MonoBehaviour
 		
 	}
 
-    public bool isTornado = false;
+
 
     //**** 1사분면에서만 정상 동작한다 ****
 	private void OnDrawGizmos()
 	{
         if (null == highestPoint) return;
-
-        //----------- debug print -----------
-        DebugWide.DrawCircle(sphereCenter.position, _radius, Color.black);
-        DebugWide.DrawLine(sphereCenter.position, anchorPointA.position, Color.gray);
-        DebugWide.DrawLine(sphereCenter.position, anchorPointB.position, Color.gray);
-
-        DebugWide.DrawLine(anchorPointA.position, highestPoint.position, Color.green);
-        DebugWide.DrawLine(anchorPointB.position, highestPoint.position, Color.green);
-        DebugWide.DrawLine(sphereCenter.position, highestPoint.position, Color.green);
-        //----------- debug print -----------
-
 
         //늘어남계수 = 원점에서 최고점까지의 길이 - 반지름 
         float centerToHighestPoint = (highestPoint.position - sphereCenter.position).magnitude;
@@ -52,19 +44,34 @@ public class DeformationCircle : MonoBehaviour
 
         Vector3 upDir = Vector3.Cross(anchorPointA.position - sphereCenter.position, anchorPointB.position - sphereCenter.position);
         upDir.Normalize();
+
+        //최고점 기준으로 좌우90,90도 최대 180도를 표현한다 
+        _initialDir = Quaternion.AngleAxis(-90f, upDir) * (highestPoint.position - sphereCenter.position);
+        _initialDir.Normalize();
+
         float angleA = Vector3.SignedAngle(_initialDir, anchorPointA.position - sphereCenter.position,  upDir);
         float angleB = Vector3.SignedAngle(_initialDir, anchorPointB.position - sphereCenter.position,   upDir);
         float angleH = Vector3.SignedAngle(_initialDir, highestPoint.position - sphereCenter.position,   upDir);
 
-        //if (0 > angleA)
-        //    angleA += 360f;
+        if (0 > angleA)
+            angleA += 360f;
+        if (0 > angleB)
+            angleB += 360f;
+        if (0 > angleH)
+            angleH += 360f;
 
-        //if (0 > angleB)
-        //    angleB += 360f;
+        Vector3 angle_M45 = _initialDir;
+        Vector3 angle_P45 = Quaternion.AngleAxis(180f, upDir) * _initialDir;
+        DebugWide.DrawLine(sphereCenter.position, angle_M45 * _radius, Color.red);
+        DebugWide.DrawLine(sphereCenter.position, angle_P45 * _radius, Color.red);
+        //----------- debug print -----------
+        DebugWide.DrawCircle(sphereCenter.position, _radius, Color.black);
+        DebugWide.DrawLine(sphereCenter.position, anchorPointA.position, Color.gray);
+        DebugWide.DrawLine(sphereCenter.position, anchorPointB.position, Color.gray);
 
-        //if (0 > angleH)
-            //angleH += 360f;
-
+        DebugWide.DrawLine(anchorPointA.position, highestPoint.position, Color.green);
+        DebugWide.DrawLine(anchorPointB.position, highestPoint.position, Color.green);
+        DebugWide.DrawLine(sphereCenter.position, highestPoint.position, Color.red);
         //----------- debug print -----------
         DebugWide.PrintText(anchorPointA.position, Color.black, "A : " + angleA);
         DebugWide.PrintText(anchorPointB.position, Color.black, "B : " + angleB);
@@ -86,6 +93,7 @@ public class DeformationCircle : MonoBehaviour
         Vector3 prevPos = sphereCenter.position;
         Vector3 tdPos = sphereCenter.position;
 
+        //최고점 기준 -90도 로 설정된 회오리를 그린다 
         count = 300;
         for (int i = 0; i < count; i++)
         {
@@ -94,43 +102,36 @@ public class DeformationCircle : MonoBehaviour
             angleD = i * 5f; //계속 증가하는 각도 .. 파도나치 수열의 소용돌이 모양이 나옴 
 
             //각도변환 : 181~359 => -179~-1 
-            angleD %= 360f;
-            if(angleD > 180f)
-                angleD -= 360f;
+            //angleD %= 360f;
+            //if(angleD > 180f)
+                //angleD -= 360f;
 
             tdPos = Quaternion.AngleAxis(angleD, upDir) * _initialDir;
 
 
             float td = (angleD * t) / angleH;
+            //float td = (angleD * 4f) / 45f;
 
 
             tdPos = sphereCenter.position + tdPos * (_radius + td);
 
             //----------- debug print -----------
-            DebugWide.DrawLine(prevPos, tdPos, Color.gray);
+            if (0 != i)
+                DebugWide.DrawLine(prevPos, tdPos, Color.gray);
             //----------- debug print -----------
 
             prevPos = tdPos;
 
         }
 
-        count = 5;
-        for (int i = 0; i < count;i++)
+        //회오리 값의 지정구간 비율값을 0~1 , 1~0 으로 변환시킨다 
+        count = 30;
+        for (int i = 0; i < count+1;i++)
         {
             
-            if(false == isTornado)
-            {
-                //angleD = Mathf.LerpAngle(angleA, angleB, i / (float)count);
-                angleD = Mathf.LerpAngle(minAngle, maxAngle, i / (float)count);
-                //angleD = Mathf.Lerp(angleA, angleB, i / (float)count);
-
-            }
-            else
-            {
-                //5도 간격으로 각도를 늘린다 
-                angleD = i * 5f; //계속 증가하는 각도 .. 파도나치 수열의 소용돌이 모양이 나옴 
-            }
-
+            //angleD = Mathf.LerpAngle(angleA, angleB, i / (float)count);
+            angleD = Mathf.LerpAngle(minAngle, maxAngle, i / (float)count);
+            //angleD = Mathf.Lerp(angleA, angleB, i / (float)count);
 
             tdPos = Quaternion.AngleAxis(angleD, upDir) * _initialDir;
 
@@ -138,10 +139,9 @@ public class DeformationCircle : MonoBehaviour
             float td = (angleD * t) / angleH;
             //DebugWide.PrintText(tdPos * _radius, Color.black, " " + td + "  " + angleD);
 
-
-            if (false && false == isTornado) 
+            if(false == isTornado)
             {
-                if(td < t)
+                if (td < t)
                 {
                     //td /= t; //0~1로 변환
                     //td *= t; //0~t 범위로 변환 
@@ -155,32 +155,28 @@ public class DeformationCircle : MonoBehaviour
                     //최고점을 기준으로 대칭형을 만들어 준다    
                     td = maxTd - td; //t ~ maxTd => (maxTd - t) ~ 0
                     td /= (maxTd - t); //1~0로 변환
-                    //td *= t; //t~0 범위값으로 바꾸어 준다 
+                                       //td *= t; //t~0 범위값으로 바꾸어 준다 
                 }
 
-                //td = UtilGS9.Interpolation.easeInQuart(0, 1f, td); //직선에 가까운 표현 가능 *
-                //td = UtilGS9.Interpolation.easeInBack(0, 1f, td); //직선에 가까운 표현 가능 **
-                //td = UtilGS9.Interpolation.easeInCirc(0, 1f, td); //직선에 가까운 표현 가능 ***
-                //td = UtilGS9.Interpolation.easeInSine(0, 1f, td); 
+                if(true == isInter)
+                {
+                    //td = UtilGS9.Interpolation.easeInQuart(0, 1f, td); //직선에 가까운 표현 가능 *
+                    td = UtilGS9.Interpolation.easeInBack(0, 1f, td); //직선에 가까운 표현 가능 **
+                    //td = UtilGS9.Interpolation.easeInCirc(0, 1f, td); //직선에 가까운 표현 가능 ***
+                    //td = UtilGS9.Interpolation.easeInSine(0, 1f, td);     
+                }
+
 
 
                 td *= t; //0~t 범위로 변환 
             }
-            //else
-            //{
-            //    float ma = 300f * 5f;
-            //    float mat = (ma * t) / angleH;
-            //    td /= mat; //0~1로 변환
-            //    td = UtilGS9.Interpolation.easeOutElastic(0, 1f, td);
-            //    td *= mat; //0~t 범위로 변환 
-            //}
+
 
 
             tdPos = sphereCenter.position +  tdPos * (_radius + td);
 
             //----------- debug print -----------
             //DebugWide.DrawLine(sphereCenter.position, tdPos, Color.red);
-            //DebugWide.PrintText(tdPos, Color.black, " " + td);
             if(0 != i) 
                 DebugWide.DrawLine(prevPos, tdPos, Color.blue);
             //----------- debug print -----------
