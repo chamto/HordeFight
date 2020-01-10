99462c64cdcbe22050433f36592a56b91f16550c
chamto <anrudco@gmail.com>
2020-01-10 23:25:17 +0900

    [ Limbs 객체 추가 :  가지들 객체 적용 준비 , 프리팹 추가 ]

---
+++ Assets/Script/Limbs.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UtilGS9;
+
+//가지 , 팔다리 
+public class Limbs : MonoBehaviour 
+{
+
+    public class Control
+    { 
+    }
+
+    public class PathCircle
+    {
+    }
+
+    static public Limbs CreateLimbs_TwoHand()
+    {
+        return null;
+    }
+
+    public GameObject CreatePrefab(string prefabPath, Transform parent, string name)
+    {
+        const string root = "Warcraft/Prefab/";
+        GameObject obj = MonoBehaviour.Instantiate(Resources.Load(root + prefabPath)) as GameObject;
+        obj.transform.SetParent(parent, false);
+        obj.transform.name = name;
+
+
+        return obj;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
