acc3ad4f8f991f8fd4ae79f46af95b73d424a606
chamto <anrudco@gmail.com>
2020-02-24 04:18:48 +0900

    [ Test_LineCollision : 사각꼴과 선분 충돌 함수 테스트 ]

--- Assets/z_Test/LineCollision/Test_LineCollision.cs
+++ Assets/z_Test/LineCollision/Test_LineCollision.cs
@@ -5,7 +5,7 @@ using UtilGS9;
 
 public class Test_LineCollision : MonoBehaviour 
 {
-
+    private bool _init = false;
     private Transform _line0_start = null;
     private Transform _line0_end = null;
     private Transform _ts0 = null;
@@ -16,9 +16,14 @@ public class Test_LineCollision : MonoBehaviour
     public float _line1_width = 0.001f;
     private Geo.Model _model_1 = new Geo.Model();
 
+    private Transform _line2_start = null;
+    private Transform _line2_end = null;
+    private Transform _ts2 = null;
+
     private void OnDrawGizmos()
     {
-        if (null == (object)_line0_start)
+        //if (null == (object)_line0_start)
+        if(false == _init)
             return;
 
         {
@@ -44,6 +49,13 @@ public class Test_LineCollision : MonoBehaviour
 
             UtilGS9.Plane plane = new UtilGS9.Plane(_line0_start.position, _line0_end.position, _line1_start.position);
             plane.Draw(5,Color.gray);
+
+            //===============
+            DebugWide.DrawLine(_line2_start.position, _line2_end.position, Color.green);
+            DebugWide.DrawLine(_line0_start.position, _line1_end.position, Color.green);
+            DebugWide.DrawLine(_line0_start.position, _line1_start.position, Color.green);
+            DebugWide.DrawLine(_line0_end.position, _line1_end.position, Color.green);
+            DebugWide.DrawCircle(_ts2.position, 0.1f, Color.green);
         }
 
     }
@@ -51,6 +63,7 @@ public class Test_LineCollision : MonoBehaviour
 	// Use this for initialization
 	void Start () 
     {
+        _init = true;
         Transform obj = Hierarchy.GetTransform(transform.parent, "obj");
         //DebugWide.LogBlue(transform.parent + "  :  "+ _obj);
         Transform line_0 = Hierarchy.GetTransform(obj, "line_0");
@@ -62,6 +75,11 @@ public class Test_LineCollision : MonoBehaviour
         _line1_start = Hierarchy.GetTransform(line_1, "start");
         _line1_end = Hierarchy.GetTransform(line_1, "end");
         _ts1 = Hierarchy.GetTransform(line_1, "ts");
+
+        Transform line_2 = Hierarchy.GetTransform(obj, "line_2");
+        _line2_start = Hierarchy.GetTransform(line_2, "start");
+        _line2_end = Hierarchy.GetTransform(line_2, "end");
+        _ts2 = Hierarchy.GetTransform(line_2, "ts");
 
 	}
 	
@@ -89,6 +107,17 @@ public class Test_LineCollision : MonoBehaviour
         }
 
         _model_1.cylinder.Set(Vector3.up, _line1_start.position, line1_width, _line1_end.position, line1_width);
+
+        //====================================
+
+        float line_t = 0f;
+        if(Tetragon.Intersect(out line_t, _line0_start.position, _line0_end.position, _line1_end.position , _line1_start.position,
+                              new LineSegment3(_line2_start.position, _line2_end.position)))
+        {
+            _ts2.position = _line2_start.position + (_line2_end.position - _line2_start.position) * line_t;
+        }
+
+
 	}
 
 }
