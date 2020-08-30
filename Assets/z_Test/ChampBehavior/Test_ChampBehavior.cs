a6cdc8cc9f603fa99dd70954d47b0f930461624d
chamto <anrudco@gmail.com>
2020-08-30 10:31:24 +0900

    [ Test_ChampBehavior :  스턴이펙트 시험3 , 회전흔들기 시험해봄 , 프리팹의 추가된 ani_spr 을 이용함 , 기존은 animator 에 x축90도 회전이 적용이 되어 있어서 회전흔들기 값 적용이 힘들었음 ]

--- Assets/z_Test/ChampBehavior/Test_ChampBehavior.cs
+++ Assets/z_Test/ChampBehavior/Test_ChampBehavior.cs
@@ -99,14 +99,14 @@ namespace HordeFight
             {
                 //_effect.Rotate(ConstV.v3_up, -3f);
 
-                Transform champ0_ani = Hierarchy.GetTransform(_champ_0.transform, "animator");
+                Transform champ0_ani = Hierarchy.GetTransform(_champ_0.transform, "ani_spr");
                 if (1 < __time_elapsed) __time_elapsed = 0f;
                 //Interpolation.CalcShakePosition(champ0_ani.transform,_champ_0.transform.position , new Vector3(0, 0, 0.1f), __time_elapsed);
-                //Interpolation.CalcShakeRotation(champ0_ani.transform, new Vector3(90f, 0 ,0) , new Vector3(90f, 20, 0), __time_elapsed);
+                Interpolation.CalcShakeRotation(champ0_ani.transform, new Vector3(0f, 0 ,0) , new Vector3(0f, 0, 90), __time_elapsed);
                 //Interpolation.CalcShakeScale(champ0_ani.transform, ConstV.v3_one,  new Vector3(0f, 0, 0.2f), __time_elapsed);
 
                 //Interpolation.CalcShakePosition(_effect, _champ_1.transform.position, new Vector3(0.1f, 0, 0.2f), __time_elapsed);
-                Interpolation.CalcShakeRotation(_effect, _champ_1.transform.eulerAngles, new Vector3(0, 45f, 0), __time_elapsed);
+                //Interpolation.CalcShakeRotation(_effect, _champ_1.transform.eulerAngles, new Vector3(0, 45f, 0), __time_elapsed);
                 //Interpolation.CalcShakeScale(_effect, _champ_1.transform.localScale, new Vector3(0.2f, 0, 0.2f), __time_elapsed);
 
                 __time_elapsed += Time.deltaTime;
