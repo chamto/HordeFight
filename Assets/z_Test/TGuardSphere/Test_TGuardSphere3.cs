1d3cf33727152503e9fc5a47d114fabf303ad705
chamto <anrudco@gmail.com>
2020-09-09 04:25:59 +0900

    [Test_TGuardSphere2 : TGS_Info 을 inner class 로 변경 ] [Test_TGuardSphere3 : MoveingSegment3 시뮬레이터를 테스트하기 위하여 씬추가 ]

--- Assets/z_Test/TGuardSphere/Test_TGuardSphere3.cs
+++ Assets/z_Test/TGuardSphere/Test_TGuardSphere3.cs
@@ -5,7 +5,7 @@ using UtilGS9;
 using System;
 
 
-public class Test_TGuardSphere2 : MonoBehaviour 
+public class Test_TGuardSphere3 : MonoBehaviour 
 {
     
     public class TGS_Info
@@ -33,8 +33,20 @@ public class Test_TGuardSphere2 : MonoBehaviour
         public Transform _Tctl_sub_start = null;
         public Transform _Tctl_sub_end = null;
 
-        public Vector3 _mt0 = ConstV.v3_zero;
-        public Vector3 _mt1 = ConstV.v3_zero;
+
+        public Vector3 _mt_min = ConstV.v3_zero;
+        public Vector3 _mt_rate = ConstV.v3_zero;
+        public Vector3 _mt_max = ConstV.v3_zero;
+
+        public LineSegment3 ToSeg_Prev()
+        {
+            return new LineSegment3(_T0_sub_start.position, _T0_sub_end.position);
+        }
+        public LineSegment3 ToSeg_Cur()
+        {
+            return new LineSegment3(_Tctl_sub_start.position, _Tctl_sub_end.position);
+        }
+
 
         public void Draw()
         {
@@ -64,10 +76,12 @@ public class Test_TGuardSphere2 : MonoBehaviour
 
             //-----------------
 
-            DebugWide.DrawCircle(_mt0, 0.02f, Color.red);
+            DebugWide.DrawCircle(_mt_min, 0.02f, Color.red);
+            DebugWide.DrawCircle(_mt_rate, 0.04f, Color.red);
+            DebugWide.DrawCircle(_mt_max, 0.06f, Color.red);
 
             //DebugWide.DrawLine(_seg1_start.position, _mt1, Color.red);
-            DebugWide.DrawLine(_T0_root_start.position, _mt1, Color.red);
+            //DebugWide.DrawLine(_T0_root_start.position, _mt_max, Color.red);
             //DebugWide.DrawLine(_T0_root_start.position, _seg0_start.position, Color.red);
 
         }
@@ -110,7 +124,7 @@ public class Test_TGuardSphere2 : MonoBehaviour
 
     TGS_Info _tgs0 = new TGS_Info();
     TGS_Info _tgs1 = new TGS_Info();
-
+    public MovingSegement3 _movTgs = new MovingSegement3();
 
 	// Use this for initialization
 	void Start () 
@@ -128,8 +142,6 @@ public class Test_TGuardSphere2 : MonoBehaviour
     void Update()
     {
         
-        //Calc_TGSvsTGS(__rate, _tgs0, _tgs1);
-        //Test_0(_tgs0, _tgs1);
 
     }
 
@@ -138,85 +150,41 @@ public class Test_TGuardSphere2 : MonoBehaviour
         
         if (true == _tgs0._init)
         {
-            //Test_0(_tgs0, _tgs1);
-            Calc_TGSvsTGS(__rate, _tgs0, _tgs1);
+            __rate = Mathf.Clamp(__rate, 0, 1f);
+            _tgs0._T1_root.position = _tgs0._T0_root.position;
+            _tgs0._T1_root.rotation = _tgs0._T0_root.rotation;
+            _tgs1._T1_root.position = _tgs1._T0_root.position;
+            _tgs1._T1_root.rotation = _tgs1._T0_root.rotation;
+
+            //------------
+
+            LineSegment3 prev_A, cur_A;
+            LineSegment3 prev_B, cur_B;
+            prev_A = _tgs0.ToSeg_Prev();
+            cur_A = _tgs0.ToSeg_Cur();
+            prev_B = _tgs1.ToSeg_Prev();
+            cur_B = _tgs1.ToSeg_Cur();
+
+            _movTgs.Find(prev_A, prev_B, cur_A, cur_B);
+
+            bool contact = _movTgs.Calc_TGuard_vs_TGuard(__rate, _tgs0._T1_root, _tgs1._T1_root);
+
+            if(true == contact)
+            {
+                _movTgs.CalcTGuard(_movTgs._minV, _movTgs._meetPt, _tgs0._T0_root.position, 
+                                   _tgs0._T0_root, _tgs0._T1_root,
+                                   prev_A, cur_B);
+
+                _tgs0._mt_min = _movTgs._minV;
+                _tgs0._mt_rate = _movTgs._meetPt;
+                _tgs0._mt_max = _movTgs._maxV;    
+            }
+
+            //------------
 
             _tgs0.Draw();
             _tgs1.Draw();    
         }
-
-    }
-
-    //테스트를 위한 최소한의 알고리즘 
-    void Test_0(TGS_Info tgs0, TGS_Info tgs1)
-    {
-
-        tgs0._T1_root.position = tgs0._T0_root.position;
-        tgs1._T1_root.position = tgs1._T0_root.position;
-        tgs0._T1_root.rotation = tgs0._T0_root.rotation;
-        tgs1._T1_root.rotation = tgs1._T0_root.rotation;
-        tgs0._mt1 = tgs0._T0_root.position;
-        tgs1._mt1 = tgs1._T0_root.position;
-
-        Vector3 pt_min, pt_max;
-        LineSegment3 ls_sub = new LineSegment3(tgs0._T0_sub_start.position, tgs0._T0_sub_end.position);
-
-        //T0_sub 선분과 Tctl_sub 선분의 시작점이 "t가드 vs 선분" 알고리즘에서는 서로만나는 점에 착안하여,
-        //"t가드 vs t가드" 알고리즘에서 서로만나지 않는 t가드sub의 시작점을 만나게 수정하였음 
-        //이때 두가지 방식으로 시작점을 만나게 할 수 있다.
-        //Tctl_sub 선분이 온전한 row 방식이 각도계산시 정상적으로 나오므로 row를 사용함 
-
-        //row
-        LineSegment3 ls_AB = new LineSegment3(tgs1._Tctl_sub_start.position, tgs0._T0_root_start.position); //row 
-        LineSegment3 ls_seg0 = new LineSegment3(tgs1._Tctl_sub_start.position, tgs1._T0_sub_end.position); //row 
-        LineSegment3 ls_seg1 = new LineSegment3(tgs1._Tctl_sub_start.position, tgs1._Tctl_sub_end.position); //row
-
-        //high
-        //LineSegment3 ls_AB = new LineSegment3(tgs1._T0_sub_start.position, tgs0._T0_root_start.position); //high
-        //LineSegment3 ls_seg0 = new LineSegment3(tgs1._T0_sub_start.position, tgs1._T0_sub_end.position); //high
-        //LineSegment3 ls_seg1 = new LineSegment3(tgs1._T0_sub_start.position, tgs1._Tctl_sub_end.position); //high
-
-        //------
-        DebugWide.DrawLine(ls_seg0.origin, ls_seg0.last, Color.cyan); //row
-        DebugWide.DrawLine(ls_seg1.origin, ls_seg1.last, Color.cyan); //high
-        DebugWide.DrawLine(ls_AB.origin, ls_AB.last, Color.red);
-        //------
-
-        LineSegment3.ClosestPoints(out pt_min, out pt_max, ls_sub, ls_seg0);
-        Vector3 dir_rootS_min = pt_min - tgs0._T0_root_start.position;
-
-        float c = ls_AB.Length(); //1# root_start , seg_start 사이의 거리 
-        float a = (dir_rootS_min).magnitude; //2# root_start , sub 선분의 접촉점 사이의 거리 
-
-        //3# seg_start , seg 선분의 접촉점 사이의 거리용
-        //코사인 제2법칙 이용
-        Vector3 up_seg1_AB = Vector3.Cross(ls_AB.direction, ls_seg1.direction);
-        float angle_cosA = UtilGS9.Geo.AngleSigned(ls_AB.direction, ls_seg1.direction, up_seg1_AB);
-        float cosA = (float)Math.Cos(angle_cosA * Mathf.Deg2Rad);
-
-
-        float dt1 = -2f * c * cosA;
-        float dt2 = c * c - a * a;
-        DebugWide.LogBlue(dt1 + "  " + dt2);
-        //이차방정식의 근의공식 이용
-        float disc = dt1 * dt1 - 4 * dt2; //판별값 : b*b-4ac
-        float root = (float)Math.Sqrt(disc);
-        float b_plus = (-dt1 + root) / 2f;
-        float b_minus = (-dt1 - root) / 2f;
-
-
-        tgs0._mt0 = pt_min;
-        tgs0._mt1 = ls_seg1.origin + ls_seg1.direction.normalized * b_minus; //임시 , 플러스인 경우도 있음 
-
-        //=======================
-
-        Vector3 up_t = Vector3.Cross(tgs0._mt0 - tgs0._T0_root_start.position, tgs0._mt1 - tgs0._T0_root_start.position);
-        float angle_t = Geo.AngleSigned(tgs0._mt0 - tgs0._T0_root_start.position, tgs0._mt1 - tgs0._T0_root_start.position, up_t);
-
-        tgs0._T1_root.rotation = Quaternion.AngleAxis(angle_t, up_t) * tgs0._T0_root.rotation;
-
-
-        DebugWide.LogBlue("c : " + c + "  a : " + a + "   b+ : " + b_plus + "  b- : " + b_minus + "  angle : " + angle_cosA + "  판별값 : " + disc);
 
     }
 
@@ -238,8 +206,8 @@ public class Test_TGuardSphere2 : MonoBehaviour
         tgs1._T1_root.position = tgs1._T0_root.position;
         tgs1._T1_root.rotation = tgs1._T0_root.rotation;
 
-        tgs0._mt1 = tgs0._T0_root.position;
-        tgs1._mt1 = tgs1._T0_root.position;
+        tgs0._mt_max = tgs0._T0_root.position;
+        tgs1._mt_max = tgs1._T0_root.position;
 
         rateAtoB = Mathf.Clamp(rateAtoB, 0, 1f);
 
@@ -363,13 +331,13 @@ public class Test_TGuardSphere2 : MonoBehaviour
         Vector3 new_dir_ls_seg1 = Quaternion.AngleAxis(angle_apply, up_seg_rate) * ls_AB.direction;
 
 
-        tgs0._mt0 = pt_min;
-        tgs0._mt1 = ls_seg1.origin + new_dir_ls_seg1.normalized * b_1;
-        DebugWide.DrawCircle(tgs0._mt1, 0.05f, Color.red);
+        tgs0._mt_min = pt_min;
+        tgs0._mt_max = ls_seg1.origin + new_dir_ls_seg1.normalized * b_1;
+        DebugWide.DrawCircle(tgs0._mt_max, 0.05f, Color.red);
         //=======================
 
-        Vector3 up_t = Vector3.Cross(tgs0._mt0 - tgs0._T0_root_start.position, tgs0._mt1 - tgs0._T0_root_start.position);
-        float angle_t = Geo.AngleSigned(tgs0._mt0 - tgs0._T0_root_start.position, tgs0._mt1 - tgs0._T0_root_start.position, up_t);
+        Vector3 up_t = Vector3.Cross(tgs0._mt_min - tgs0._T0_root_start.position, tgs0._mt_max - tgs0._T0_root_start.position);
+        float angle_t = Geo.AngleSigned(tgs0._mt_min - tgs0._T0_root_start.position, tgs0._mt_max - tgs0._T0_root_start.position, up_t);
 
         tgs0._T1_root.rotation = Quaternion.AngleAxis(angle_t, up_t) * tgs0._T0_root.rotation;
 
@@ -380,135 +348,5 @@ public class Test_TGuardSphere2 : MonoBehaviour
 
     }
 
-
-    /*
-    private Vector3 __mt_0, __mt_1;
-    public void Update(float rateAtoB, Vector3 fixedOriginPt_a, Vector3 fixedOriginPt_b)
-    {
-        _T0_root_start.position = fixedOriginPt_a;
-        _seg0_start.position = fixedOriginPt_b;
-        _T1.position = _T0.position;
-        _seg1.position = _seg0.position;
-        rateAtoB = Mathf.Clamp(rateAtoB, 0, 1f);
-
-        //1# root_start , seg_start 사이의 거리 
-        LineSegment3 ls_AB = new LineSegment3(_seg0_start.position, _T0_root_start.position);
-        float c = (_T0_root_start.position - _seg0_start.position).magnitude;
-
-        //2# root_start , sub 선분의 접촉점 사이의 거리 
-        Vector3 pt_min, pt_max;
-        LineSegment3 ls_sub = new LineSegment3(_T0_sub_start.position, _T0_sub_end.position);
-        LineSegment3 ls_seg0 = new LineSegment3(_seg0_start.position, _seg0_end.position);
-        LineSegment3.ClosestPoints(out pt_min, out pt_max, ls_sub, ls_seg0);
-        Vector3 dir_rootS_min = pt_min - _T0_root_start.position;
-        float a = (dir_rootS_min).magnitude;
-
-        //근의공식 부호 결정 
-        float value_sign = Vector3.Dot(dir_rootS_min, -ls_AB.direction);
-        if (value_sign > 0) value_sign = -1; else value_sign = 1;
-
-        //3# seg_start , seg 선분의 접촉점 사이의 거리용
-        Vector3 dir_ptmin = pt_min - _seg0_start.position;
-        LineSegment3 ls_seg1 = new LineSegment3(_seg1_start.position, _seg1_end.position);
-        Vector3 up_AB_seg1 = Vector3.Cross(ls_AB.direction, ls_seg1.direction);
-        //Vector3 up_AB_ptmin = Vector3.Cross(ls_AB.direction, dir_ptmin);
-
-        //---------------------------------------
-        //선분1 이 최소선분 위에 있으면 최소선분으로 고정함   
-        Vector3 t1 = Vector3.Cross(ls_seg0.direction, dir_ptmin);
-        Vector3 t2 = Vector3.Cross(dir_ptmin, ls_seg1.direction);
-        if (Vector3.Dot(t1, t2) < 0)
-        {
-            rateAtoB = 0;
-        }
-
-        //---------------------------------------
-        //코사인 제2법칙 이용
-        float dt2 = c * c - a * a;
-
-        //---------------------------------------
-        //비율값으로 새로운 선분구함 
-        Vector3 pt_seg1 = ls_seg1.origin + ls_seg1.direction.normalized * dir_ptmin.magnitude;
-        Vector3 pt_rate = pt_min + (pt_seg1 - pt_min) * rateAtoB;
-
-        //-----------------
-        Vector3 dir_seg_rate = pt_rate - ls_seg0.origin;
-        Vector3 up_seg_rate = Vector3.Cross(ls_AB.direction, dir_seg_rate);
-        float angle_seg_rate = UtilGS9.Geo.AngleSigned(ls_AB.direction, dir_seg_rate, up_seg_rate);
-
-        //---------------------------------------
-        //float cosA = (float)Math.Cos(angle_cosA * Mathf.Deg2Rad);
-        float cosA = (float)Math.Cos(angle_seg_rate * Mathf.Deg2Rad);
-        float dt1 = -2f * c * cosA; //dt1 : -2cCosA
-
-        //이차방정식의 근의공식 이용 , disc = 판별값 
-        float disc = dt1 * dt1 - 4 * dt2;
-        float b_1 = (-dt1 + value_sign * (float)Math.Sqrt(disc)) / 2f; //가까운점 
-
-        float angle_apply = angle_seg_rate;
-
-        //---
-        //비율적용된 각도값이 아닌 원래 각도값의 판별값을 이용한다.
-        //비율적용된 각도값의 판별값을 사용하면 비율값에 따라 최대범위가 늘어진다
-        float angle_cosA = UtilGS9.Geo.AngleSigned(ls_AB.direction, ls_seg1.direction, up_AB_seg1);
-        cosA = (float)Math.Cos(angle_cosA * Mathf.Deg2Rad);
-        dt1 = -2f * c * cosA;
-        float disc2 = dt1 * dt1 - 4 * dt2;
-        //판별값이 0 보다 작다면 해가 없는 상태이다 
-        //if (disc < 0 || b_1 < 0)
-        if (disc2 < 0 || b_1 < 0)
-        {
-
-            //---------------------------------------
-            //# 이동가능 최대값 구함 
-            float cosA_max = (float)Math.Sqrt(dt2 / (c * c));
-            cosA_max = Mathf.Clamp(cosA_max, -1f, 1f);
-            float angle_cosA_max = (float)Math.Acos(cosA_max);
-            angle_cosA_max = angle_cosA_max * Mathf.Rad2Deg;
-            float b_max = c * cosA_max; //최대각도에서의 b길이 
-
-            //dir_seg_max 를 구하는데 up_AB_seg1 이 값을 사용하여 근사값 사용 
-            Vector3 dir_seg_max = Quaternion.AngleAxis(angle_cosA_max, up_AB_seg1) * ls_AB.direction;
-            pt_max = ls_seg1.origin + dir_seg_max.normalized * b_max;
-
-            pt_rate = pt_min + (pt_max - pt_min) * rateAtoB;
-            dir_seg_rate = pt_rate - ls_seg0.origin;
-            up_seg_rate = Vector3.Cross(ls_AB.direction, dir_seg_rate);
-            angle_seg_rate = UtilGS9.Geo.AngleSigned(ls_AB.direction, dir_seg_rate, up_seg_rate);
-            cosA = (float)Math.Cos(angle_seg_rate * Mathf.Deg2Rad);
-            dt1 = -2f * c * cosA; //dt1 : -2cCosA
-            //이차방정식의 근의공식 이용 , disc = 판별값 
-            disc = dt1 * dt1 - 4 * dt2;
-            if (disc < 0) disc = 0;
-            b_1 = (-dt1 + value_sign * (float)Math.Sqrt(disc)) / 2f; //가까운점 
-
-            angle_apply = angle_seg_rate;
-            //---------------------------------------
-
-            //최대각도로 다시 지정 
-            //angle_apply = angle_cosA_max; 
-            //b_1 = b_max;
-
-        }
-
-
-        //Vector3 new_dir_ls_seg1 = Quaternion.AngleAxis(angle_apply, up_AB_seg1) * ls_AB.direction;
-        Vector3 new_dir_ls_seg1 = Quaternion.AngleAxis(angle_apply, up_seg_rate) * ls_AB.direction;
-
-
-        __mt_0 = pt_min;
-        __mt_1 = ls_seg1.origin + new_dir_ls_seg1.normalized * b_1;
-
-        //=======================
-
-        Vector3 up_t = Vector3.Cross(__mt_0 - _T0_root_start.position, __mt_1 - _T0_root_start.position);
-        float angle_t = Geo.AngleSigned(__mt_0 - _T0_root_start.position, __mt_1 - _T0_root_start.position, up_t);
-
-        _T1_root.rotation = Quaternion.AngleAxis(angle_t, up_t) * _T0_root.rotation;
-
-        //DebugWide.LogBlue("c : " + c + "  a : " + a + "   b_one : " + b_1 + "  new_angle : " + 0 + "  cosA :" + cosA + "  disc :" + disc + "  value_sign :" + value_sign);
-
-    }
-    //*/
 
 }
