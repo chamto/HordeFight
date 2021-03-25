8b8447bd8525e431891132374fcabf41997907cf
chamto <anrudco@gmail.com>
2021-03-26 00:32:14 +0900

    [ DebugWide : DrawCube : rotation 이 추가된 함수 추가 ]

--- Assets/Script/0_Utility/DebugWide.cs
+++ Assets/Script/0_Utility/DebugWide.cs
@@ -182,10 +182,58 @@ public class DebugWide
 #if UNITY_EDITOR
         Gizmos.color = cc;
         Gizmos.DrawWireCube(pos, size);
+        Mesh mesh = new Mesh();
 #endif
     }
 
+    //ref : http://ilkinulas.github.io/development/unity/2016/04/30/cube-mesh-in-unity3d.html
+    static public Mesh CreateCubeMesh()
+    {
+        Vector3[] vertices = {
+            new Vector3 (0, 0, 0),
+            new Vector3 (1, 0, 0),
+            new Vector3 (1, 1, 0),
+            new Vector3 (0, 1, 0),
+            new Vector3 (0, 1, 1),
+            new Vector3 (1, 1, 1),
+            new Vector3 (1, 0, 1),
+            new Vector3 (0, 0, 1),
+        };
 
+        int[] triangles = {
+            0, 2, 1, //face front
+            0, 3, 2,
+            2, 3, 4, //face top
+            2, 4, 5,
+            1, 2, 5, //face right
+            1, 5, 6,
+            0, 7, 4, //face left
+            0, 4, 3,
+            5, 4, 7, //face back
+            5, 7, 6,
+            0, 6, 7, //face bottom
+            0, 1, 6
+        };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+
+        return mesh;
+    }
+    static Mesh __cubeMesh = null;
+
+    static public void DrawCube(Vector3 pos, Quaternion rotation, Vector3 size, Color cc)
+    {
+#if UNITY_EDITOR
+        Gizmos.color = cc;
+
+        if (null == __cubeMesh) __cubeMesh = CreateCubeMesh();
+        Gizmos.DrawWireMesh(__cubeMesh, pos, rotation, size);
+#endif
+    }
 
     static public void PrintText(Vector3 pos, Color cc, string text)
     {
