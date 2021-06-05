614ef7b5ee33ae2d7b661d3c434eaa16b9dcb10e
chamto <anrudco@gmail.com>
2021-06-06 07:56:07 +0900

    [ 주석 추가 ]

--- Assets/z_Test/Physics/Cyclone/Core.cs
+++ Assets/z_Test/Physics/Cyclone/Core.cs
@@ -137,6 +137,7 @@ namespace Cyclone
          * Calculates and returns the vector product of this vector
          * with the given vector.
          */
+         // %연산자로 외적을 계산 
         public static Vector3 operator %(Vector3 v1, Vector3 v2)
         {
             return new Vector3(v1.y * v2.z - v1.z * v2.y,
@@ -158,6 +159,7 @@ namespace Cyclone
          * Calculates and returns the scalar product of this vector
          * with the given vector.
          */
+         // * 연산자로 내적을 계산 
         public static float operator *(Vector3 v1, Vector3 v2)
         {
             return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
