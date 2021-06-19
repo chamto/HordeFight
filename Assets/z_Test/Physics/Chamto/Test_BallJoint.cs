322ce11c1f288a8a3046001e5c5681f1a2f31543
chamto <anrudco@gmail.com>
2021-06-20 03:31:35 +0900

    [ Chamto.Test_BallJoint : 철퇴가 멈췄을때의 조화운동 표현 , 약간 부자연스럽다 ]

--- Assets/z_Test/Physics/Chamto/Test_BallJoint.cs
+++ Assets/z_Test/Physics/Chamto/Test_BallJoint.cs
@@ -15,6 +15,8 @@ namespace Test_BallJoint
         ParticleRod[] cables = new ParticleRod[CABLE_COUNT];
         //ParticleCable[] cables = new ParticleCable[CABLE_COUNT];
 
+        ParticleRodConstraint constraint = new ParticleRodConstraint();
+
         public Transform _tr_center = null;
         public Transform _tr_dir = null;
 
@@ -34,6 +36,10 @@ namespace Test_BallJoint
             {
                 balls[i] = new Point_mass(); 
             }
+
+            constraint.anchor = _tr_center.position;
+            constraint.particle = balls[0];
+            constraint.length = 5f;
 
             for (int i = 0; i < CABLE_COUNT; i++)
             {
@@ -64,8 +70,8 @@ namespace Test_BallJoint
                 balls[i].damping = Damping;
             }
             balls[0].linearAcceleration = new Vector3(0, 0, 0); //조인트를 조종하는 공에는 가속도가 설정되면 안됨 , 조금씩 떨어지는 버그 발생 
-            balls[3].radius = 1f;
-            balls[3].mass = 2;
+            //balls[3].radius = 1f;
+            //balls[3].mass = 2;
         }
 
 
@@ -73,8 +79,159 @@ namespace Test_BallJoint
         Vector3 _dir = Vector3.forward;
         public float _angle = 15;
         public float _force = 30;
+        public float _norm_force = 10f;
+
         void Update()
         {
+            //constraint.anchor = _tr_center.position;
+            balls[0].position = _tr_center.position + _dir * 5f;
+
+            //철퇴 회전시키기 
+            if (Input.GetKey(KeyCode.A))
+            {
+                //자동회전 
+                _dir = Quaternion.AngleAxis(_angle, Vector3.up) * _dir;
+
+                //직접조종
+                //_dir = _tr_dir.position - _tr_center.position;
+                //_dir.Normalize();
+
+                //balls[0].position = _tr_center.position + _dir * 5f;
+
+                //충돌효과가 난것처럼 따라한다 
+                Vector3 v = _dir * _force;
+                Vector3 a = v / TIME_D;
+                for (int i = 1; i < BALL_COUNT; i++)
+                {
+                    balls[i].linearVelocity = v;
+                    balls[i].forces = a * balls[i].mass; //힘을 적용하면 철퇴가 자연스럽게 가속하는 모습을 볼 수 있다
+
+                }
+            }
+            //철퇴회전이 멈추면 중력이 적용되게 한다 
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                for (int i = 1; i < BALL_COUNT; i++)
+                {
+                    //조화운동을 하지 않아 부자연스럽게 보인다 
+                    //balls[i].linearVelocity = new Vector3(0, 0, -15) * balls[i].mass; //임의의 적당한 값을 넣는다 
+
+                }
+
+                //시험코드 
+                //balls[0].position = _tr_center.position + _dir * 5f;
+                //for (int i = 0; i < CABLE_COUNT; i++)
+                //{
+                //    Contact contact;
+                //    if (cables[i].GetContact(out contact))
+                //    {
+                //        contact.resolveVelocity(TIME_D);
+                //    }
+                //}
+            }
+
+            //!!!
+            for (int i = 1; i < BALL_COUNT; i++)
+            {
+                Vector3 contactNormal = (balls[i - 1].position - balls[i].position).normalized;
+
+                Vector3 relativeVelocity = balls[i-1].linearVelocity - balls[i].linearVelocity;
+
+                //Vector3 relativeVelocity =  - balls[i].linearVelocity;
+
+                float sep = Vector3.Dot(relativeVelocity, contactNormal);
+
+                balls[i].linearVelocity += sep * contactNormal * _norm_force;
+
+                //balls[i].linearVelocity += contactNormal * _norm_force;
+            }
+
+            //222
+            //for (int i = 1; i < BALL_COUNT; i++)
+            //{
+            //    Vector3 contactNormal = (balls[i].position - balls[i-0].position).normalized;
+
+            //    Vector3 relativeVelocity = balls[i - 1].linearVelocity - balls[i].linearVelocity;
+
+            //    //Vector3 relativeVelocity =  - balls[i].linearVelocity;
+
+            //    float sep = Vector3.Dot(relativeVelocity, contactNormal);
+
+            //    balls[i-1].linearVelocity += sep * contactNormal * _norm_force;// * balls[i].getInverseMass();
+            //    balls[i].linearVelocity += sep * -contactNormal * _norm_force;// * balls[i].getInverseMass();
+            //    //balls[i].linearVelocity += contactNormal * _norm_force;
+            //}
+
+            //for (int i = 1; i < BALL_COUNT; i++)
+            //{
+            //    Vector3 contactNormal = (balls[i].position - balls[i-1].position).normalized;
+
+            //    Vector3 relativeVelocity = balls[i - 1].linearVelocity - balls[i].linearVelocity;
+
+
+            //    float sep = Vector3.Dot(relativeVelocity, contactNormal);
+
+            //    balls[i].linearVelocity -= sep * contactNormal * _norm_force;// * balls[i].getInverseMass();
+
+            //}
+
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Init();
+            }
+
+            for (int i = 0; i < BALL_COUNT; i++)
+            {
+                balls[i].integrate(TIME_D);
+            }
+
+            //===============================
+            //for (int i = 1; i < BALL_COUNT; i++)
+            //{
+            //    Vector3 contactNormal = (balls[i].position - balls[i-1].position).normalized;
+
+            //    balls[i].position = balls[i-1].position + contactNormal * 3;
+            //}
+            //===============================
+
+            Contact contact;
+
+            for (int i = 0; i < CABLE_COUNT; i++)
+            {
+                if (cables[i].GetContact(out contact))
+                {
+
+                    contact.particle[1].position -= contact.contactNormal * contact.penetration;
+                    //DebugWide.LogBlue(i + "  " + contact.particle[1].position + "  " + contact.contactNormal + "  " + contact.penetration);
+                }
+            }
+            //===============================
+            // Create the ground plane data
+            CollisionPlane planeZ = new CollisionPlane();
+            planeZ.direction = new Vector3(0, 0, 1); //정규화된 값이어야 함 
+            planeZ.offset = 0;
+            planeZ.mass = 1;
+            planeZ.elasticity = 1f;
+
+            for (int i = 0; i < BALL_COUNT; i++)
+            {
+                if (CollisionDetector.sphereAndHalfSpace(balls[i], planeZ, out contact))
+                {
+
+                    balls[i].position += contact.contactNormal * contact.penetration; //겹침 계산 
+
+
+                    HandleCollision(ref balls[i], ref planeZ, TIME_D);
+
+                }
+            }
+        }
+
+        void Update2()
+        {
+            constraint.anchor = _tr_center.position;
+
             //철퇴 회전시키기 
             if (Input.GetKey(KeyCode.A))
             {
@@ -97,17 +254,6 @@ namespace Test_BallJoint
 
                 }
             }
-            //철퇴회전이 멈추면 중력이 적용되게 한다 
-            if (Input.GetKeyUp(KeyCode.A))
-            {
-                for (int i = 1; i < BALL_COUNT; i++)
-                {
-                    //조화운동을 하지 않아 부자연스럽게 보인다 
-                    balls[i].linearVelocity = new Vector3(0, 0, -15) * balls[i].mass; //임의의 적당한 값을 넣는다 
-
-                }
-            }
-
 
 
 
@@ -121,11 +267,13 @@ namespace Test_BallJoint
                 balls[i].integrate(TIME_D);
             }
 
-
-
             generateContacts();
+            resolveContacts();
+
+
         }
 
+        List<Contact> _contacts = new List<Contact>();
         void generateContacts()
         {
             // Create the ground plane data
@@ -133,17 +281,22 @@ namespace Test_BallJoint
             planeZ.direction = new Vector3(0, 0, 1); //정규화된 값이어야 함 
             planeZ.offset = 0;
             planeZ.mass = 1;
-            planeZ.elasticity = 1;
+            planeZ.elasticity = 0.2f;
 
-
+            _contacts.Clear();
             Contact contact;
+
+            if (constraint.GetContact(out contact))
+            {
+                _contacts.Add(contact);
+            }
 
             for (int i = 0; i < CABLE_COUNT; i++)
             {
-                if(cables[i].GetContact(out contact) )
+                if (cables[i].GetContact(out contact))
                 {
+                    _contacts.Add(contact);
 
-                    contact.particle[1].position -= contact.contactNormal * contact.penetration;
                     //DebugWide.LogBlue(i + "  " + contact.particle[1].position + "  " + contact.contactNormal + "  " + contact.penetration);
                 }
             }
@@ -152,15 +305,46 @@ namespace Test_BallJoint
             {
                 if (CollisionDetector.sphereAndHalfSpace(balls[i], planeZ, out contact))
                 {
-                
-                    balls[i].position += contact.contactNormal * contact.penetration; //겹침 계산 
-
-
-                    HandleCollision(ref balls[i], ref planeZ, TIME_D);
-
+                    _contacts.Add(contact);
                 }
             }
 
+        }
+
+        public void resolveContacts()
+        {
+            Contact contact;
+            for (int i = 0; i < _contacts.Count; i++)
+            {
+
+                {
+                    contact = _contacts[i];
+
+                    //Vector3 relativeVelocity = contact.particle[0].linearVelocity - contact.particle[1].linearVelocity;
+                    //float sep = Vector3.Dot(relativeVelocity, contact.contactNormal);
+                    //contact.particle[0].linearVelocity += sep * contact.contactNormal;
+                    //contact.particle[1].linearVelocity += sep * -contact.contactNormal;
+                    //if (0 == i)
+                    //{
+                    //    Vector3 movePerIMass = contact.contactNormal * (contact.penetration / 1);
+                    //    //contact.particle[0].position += movePerIMass * 0.5f;
+                    //    contact.particle[1].position -= movePerIMass;
+                    //}
+                    //else
+                    //{
+                    //    Vector3 movePerIMass = contact.contactNormal * (contact.penetration / 2);
+                    //    contact.particle[0].position += movePerIMass;
+                    //    contact.particle[1].position -= movePerIMass;
+                    //}
+
+
+                    //contact.resolveInterpenetration(TIME_D);
+                    contact.resolve(TIME_D);
+
+                    //contact.particle[1].position -= contact.contactNormal * contact.penetration;
+                    //DebugWide.LogBlue(i + "  " + contact.particle[1].position + "  " + contact.contactNormal + "  " + contact.penetration);
+                }
+            }
         }
 
         private void OnDrawGizmos()
@@ -350,6 +534,81 @@ namespace Test_BallJoint
         }
     }
 
+    public class ParticleConstraint 
+    {
+
+        /**
+        * Holds the particles connected by this constraint.
+        */
+        public Point_mass particle;
+
+        /**
+         * The point to which the particle is anchored.
+         */
+        public Vector3 anchor;
+
+
+        /**
+        * Returns the current length of the link.
+        */
+        protected float currentLength()
+        {
+            Vector3 relativePos = particle.position - anchor;
+            return relativePos.magnitude;
+        }
+
+    }
+
+    public class ParticleRodConstraint : ParticleConstraint
+    {
+
+        /**
+        * Holds the length of the rod.
+        */
+        public float length;
+
+
+
+        public bool GetContact(out Contact contact)
+        {
+            contact = new Contact();
+
+            // Find the length of the rod
+            float currentLen = currentLength();
+
+            // Check if we're over-extended
+            if (Misc.IsZero(currentLen - length))
+            {
+                return false;
+            }
+
+            // Otherwise return the contact
+            contact.particle[0] = particle;
+            contact.particle[1] = null;
+
+            // Calculate the normal
+            Vector3 normal = anchor - particle.position;
+            normal.Normalize();
+
+            // The contact normal depends on whether we're extending or compressing
+            if (currentLen > length)
+            {
+                contact.contactNormal = normal;
+                contact.penetration = currentLen - length;
+            }
+            else
+            {
+                contact.contactNormal = normal * -1;
+                contact.penetration = length - currentLen;
+            }
+
+            // Always use zero restitution (no bounciness)
+            contact.restitution = 0;
+
+            return true;
+        }
+    }
+
     public struct CollisionPlane
     {
 
@@ -389,6 +648,123 @@ namespace Test_BallJoint
         public float penetration;
 
         public float restitution;
+
+        public Vector3[] particleMovement = new Vector3[2];
+
+        public void resolve(float duration)
+        {
+            resolveVelocity(duration);
+            resolveInterpenetration(duration);
+        }
+
+        public float calculateSeparatingVelocity()
+        {
+            Vector3 relativeVelocity = particle[0].linearVelocity;
+            if (null != particle[1]) relativeVelocity -= particle[1].linearVelocity;
+            return Vector3.Dot( relativeVelocity , contactNormal); //내적표현 , * 연산자가 재정의 되어 있음 
+        }
+
+
+        /**
+         * Handles the impulse calculations for this collision.
+         */
+        public void resolveVelocity(float duration)
+        {
+            // Find the velocity in the direction of the contact
+            float separatingVelocity = calculateSeparatingVelocity();
+
+            // Check if it needs to be resolved
+            if (separatingVelocity > 0)
+            {
+                // The contact is either separating, or stationary - there's
+                // no impulse required.
+                return;
+            }
+
+            // Calculate the new separating velocity
+            float newSepVelocity = -separatingVelocity * restitution;
+
+            // Check the velocity build-up due to acceleration only
+            Vector3 accCausedVelocity = particle[0].linearAcceleration;
+            if (null != particle[1]) accCausedVelocity -= particle[1].linearAcceleration;
+            float accCausedSepVelocity = Vector3.Dot(accCausedVelocity , contactNormal) * duration;
+
+            // If we've got a closing velocity due to acceleration build-up,
+            // remove it from the new separating velocity
+            if (accCausedSepVelocity < 0)
+            {
+                newSepVelocity += restitution * accCausedSepVelocity;
+
+                // Make sure we haven't removed more than was
+                // there to remove.
+                if (newSepVelocity < 0) newSepVelocity = 0;
+            }
+
+            float deltaVelocity = newSepVelocity - separatingVelocity;
+
+            // We apply the change in velocity to each object in proportion to
+            // their inverse mass (i.e. those with lower inverse mass [higher
+            // actual mass] get less change in velocity)..
+            float totalInverseMass = particle[0].getInverseMass();
+            if (null != particle[1]) totalInverseMass += particle[1].getInverseMass();
+
+            // If all particles have infinite mass, then impulses have no effect
+            if (totalInverseMass <= 0) return;
+
+            // Calculate the impulse to apply
+            float impulse = deltaVelocity / totalInverseMass;
+
+            // Find the amount of impulse per unit of inverse mass
+            Vector3 impulsePerIMass = contactNormal * impulse;
+
+            // Apply impulses: they are applied in the direction of the contact,
+            // and are proportional to the inverse mass.
+            particle[0].linearVelocity = (particle[0].linearVelocity +
+                impulsePerIMass * particle[0].getInverseMass()
+                );
+            if (null != particle[1])
+            {
+                // Particle 1 goes in the opposite direction
+                particle[1].linearVelocity = (particle[1].linearVelocity +
+                    impulsePerIMass * -particle[1].getInverseMass()
+                    );
+            }
+        }
+
+        public void resolveInterpenetration(float duration)
+        {
+            // If we don't have any penetration, skip this step.
+            if (penetration <= 0) return;
+
+            // The movement of each object is based on their inverse mass, so
+            // total that.
+            float totalInverseMass = particle[0].getInverseMass();
+            if (null != particle[1]) totalInverseMass += particle[1].getInverseMass();
+
+            // If all particles have infinite mass, then we do nothing
+            if (totalInverseMass <= 0) return;
+
+            // Find the amount of penetration resolution per unit of inverse mass
+            Vector3 movePerIMass = contactNormal * (penetration / totalInverseMass);
+
+            // Calculate the the movement amounts
+            particleMovement[0] = movePerIMass * particle[0].getInverseMass();
+            if (null != particle[1])
+            {
+                particleMovement[1] = movePerIMass * -particle[1].getInverseMass();
+            }
+            else
+            {
+                particleMovement[1] = Vector3.zero;
+            }
+
+            // Apply the penetration resolution
+            particle[0].position = (particle[0].position + particleMovement[0]);
+            if (null != particle[1])
+            {
+                particle[1].position = (particle[1].position + particleMovement[1]);
+            }
+        }
     }
 
     public class CollisionDetector
@@ -407,6 +783,9 @@ namespace Test_BallJoint
 
             if (ballDistance >= 0) return false;
 
+            contact.particle[0] = sphere;
+            contact.particle[1] = null;
+            contact.restitution = plane.elasticity;
 
             contact.contactNormal = plane.direction;
             contact.penetration = -ballDistance;
@@ -429,10 +808,14 @@ namespace Test_BallJoint
         public float damping;
 
 
+        public float getInverseMass()
+        {
+            return 1f / mass; 
+        }
+
         public float _MAX_VELO = 100;
         public void integrate(float changeInTime)
         {
-
 
             // Work out the acceleration from the force
             Vector3 resultingAcc = linearAcceleration;
