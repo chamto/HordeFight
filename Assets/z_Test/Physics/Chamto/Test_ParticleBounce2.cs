bd24df62d67b6f228bcbdcf7bfc2c24d9a763ea1
chamto <anrudco@gmail.com>
2021-12-12 02:48:16 +0900

    [ Test_ParticleBounce2 : 접촉정보를 미리 수집후 충돌처리 하도록 변경 ,  기존방식은 접촉정보를 찾은 동시에 충돌처리를 하여 충돌처리를 할때마다 접촉정보가 변경되는 문제가 있었음 ]

--- Assets/z_Test/Physics/Chamto/Test_ParticleBounce2.cs
+++ Assets/z_Test/Physics/Chamto/Test_ParticleBounce2.cs
@@ -13,6 +13,7 @@ namespace Test_ParticleBounce2
     {
         public const int COUNT = 50;
         public Point_mass[] particles = null;
+        private List<Contact> _contacts = new List<Contact>();
 
         //-----------------------
         public float mass_0 = 1;
@@ -60,7 +61,8 @@ namespace Test_ParticleBounce2
                 particles[i].radius = 1;
                 particles[i].linearVelocity = Vector3.zero;
                 particles[i].linearAcceleration = Vector3.zero;
-                particles[i].location = Misc.GetDir8_Random_AxisY() * 0.5f;
+                //particles[i].location = Misc.GetDir8_Random_AxisY() * 0.5f;
+                particles[i].location = Vector3.zero;
                 particles[i].forces = new Vector3(0, 0.0f, 0.0f);
                 particles[i].friction = Friction_all;
                 particles[i].isStatic = static_all;
@@ -80,8 +82,8 @@ namespace Test_ParticleBounce2
 
         }
 
-        //private bool forceApplied = false;
-        void Update()
+
+        void Update_0()
         {
 
             if (Input.GetKeyDown(KeyCode.R))
@@ -93,10 +95,10 @@ namespace Test_ParticleBounce2
             float timeInterval = 0.05f; //물리시뮬시 Time.delta 넣으면 안됨 , 디버그 코드에 의한 프레임드롭시 결과가 이상해짐 
             for (int i = 0; i < COUNT; i++)
             {
-                particles[i].Update(timeInterval);
+                particles[i].Intergrate(timeInterval);
             }
 
-
+            //접촉정보를 미리 수집하지 않아 충돌처리시 마다 접촉정보가 달라지는 문제가 있음 
             //----------------------------------------------------
             Point_mass src, dst;
             //한집합의 원소로 중복되지 않는 한쌍 만들기  
@@ -114,6 +116,62 @@ namespace Test_ParticleBounce2
 
         }
 
+        //public bool __oneTime = false;
+        void Update()
+        {
+
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Init();
+            }
+
+            //if (false == __oneTime) return;
+            //__oneTime = false;
+
+            //1초 / 30프레임 = 0.033
+            float timeInterval = 0.05f; //물리시뮬시 Time.delta 넣으면 안됨 , 디버그 코드에 의한 프레임드롭시 결과가 이상해짐 
+            for (int i = 0; i < COUNT; i++)
+            {
+                particles[i].Intergrate(timeInterval);
+            }
+
+
+            GenerateContacts(); //접촉정보를 미리 수집함
+            ResolveContacts(timeInterval); //수집한 접촉정보에 따라 충돌처리함 
+
+        }
+
+        public void GenerateContacts()
+        {
+            //----------------------------------------------------
+            _contacts.Clear(); //접촉정보를 모두 비운다 
+
+            Point_mass src, dst;
+            Contact contact = null;
+            //한집합의 원소로 중복되지 않는 한쌍 만들기  
+            for (int i = 0; i < particles.Length - 1; i++)
+            {
+                for (int j = i + 1; j < particles.Length; j++)
+                {
+
+                    src = particles[i];
+                    dst = particles[j];
+                    CollisionDetector.SphereAndSphere(src, dst, out contact);
+                    _contacts.Add(contact);
+                }
+            }
+            //----------------------------------------------------
+        }
+
+        public void ResolveContacts(float timeInterval)
+        { 
+            for(int i=0;i<_contacts.Count;i++)
+            {
+                _contacts[i].Resolve(timeInterval);
+            }
+
+        }
 
         public void OnDrawGizmos()
 		{
@@ -193,16 +251,6 @@ namespace Test_ParticleBounce2
                 float len_bt_src = len_bitween * rate_src;
                 float len_bt_dst = len_bitween * rate_dst;
 
-                //2.완전겹친상태 
-                //if (float.Epsilon >= len_dstTOsrc)
-                //{
-                //    contactNormal = Misc.GetDir8_Random_AxisY();
-                //    len_dstTOsrc = 1f;
-                //    len_bt_src = r_sum * 0.5f;
-                //    len_bt_dst = r_sum * 0.5f;
-                //}
-
-
                 src.location += contactNormal * len_bt_src;
                 dst.location += -contactNormal * len_bt_dst;
             }
@@ -281,6 +329,185 @@ namespace Test_ParticleBounce2
 
     }
 
+    public class CollisionDetector
+    {
+
+        public static bool SphereAndSphere(Point_mass pm_0, Point_mass pm_1, out Contact contact)
+        {
+            contact = new Contact();
+
+
+            Vector3 dir_dstTOsrc = pm_0.location - pm_1.location;
+            float sqr_dstTOsrc = dir_dstTOsrc.sqrMagnitude;
+            float r_sum = (pm_0.radius + pm_1.radius);
+            float sqr_r_sum = r_sum * r_sum;
+
+
+            //1.두 캐릭터가 겹친상태 
+            if (sqr_dstTOsrc < sqr_r_sum)
+            {
+                Vector3 contactNormal = ConstV.v3_zero;
+                float len_dstTOsrc = (float)Math.Sqrt(sqr_dstTOsrc);
+                float penetration = (r_sum - len_dstTOsrc);
+
+                if (float.Epsilon >= len_dstTOsrc)
+                {
+                    //완전겹친상태
+                    contactNormal = Misc.GetDir8_Random_AxisY();
+                    penetration = r_sum * 0.5f; //반지름합의 평균을 침투길이로 사용한다. 이 처리를 안하면 비정상적으로 퍼지게 된다 
+                }
+                else
+                {
+                    contactNormal = (dir_dstTOsrc) / len_dstTOsrc;
+                }
+
+                contact.pm_0 = pm_0;
+                contact.pm_1 = pm_1;
+                contact.restitution = (pm_0.elasticity + pm_1.elasticity) * 0.5f; //평균값
+
+                contact.contactNormal = contactNormal;
+                contact.penetration = penetration;
+
+                return true;
+            }
+
+            //2.안겹침 
+            return false;
+        }
+    }
+
+    public class Contact
+    {
+        public Point_mass pm_0 = new Point_mass();
+        public Point_mass pm_1 = new Point_mass();
+
+        //public Vector3 contactPoint;
+
+        public Vector3 contactNormal;
+
+
+        public float penetration;
+
+        public float restitution; //반발계수
+
+        //public Vector3[] particleMovement = new Vector3[2];
+
+        public void Resolve(float duration)
+        {
+            ResolveVelocity(duration);
+            ResolveInterpenetration(duration);
+        }
+
+
+        public void ResolveVelocity(float timeInterval)
+        {
+
+            Vector3 initVelocity0 = pm_0.linearVelocity;
+            Vector3 initVelocity1 = pm_1.linearVelocity;
+
+            float dotVelocity0 = Vector3.Dot(initVelocity0, contactNormal);
+            float dotVelocity1 = Vector3.Dot(initVelocity1, contactNormal);
+
+
+            // Find the average coefficent of restitution.
+            //float averageE = (pm_0.elasticity + pm_1.elasticity) / 2f;
+            float averageE = restitution;
+
+            //탄성력이 1 , 질량이 1 이라고 가정할시,
+            //((1 - (1 * 1)) * v0 + ((1 + 1) * 1 * v1) / 1 + 1
+            //(2 * v1) / 2 = v1
+            // Calculate the final velocities.
+            float finalVelocity0 =
+                (((pm_0.mass -
+                   (averageE * pm_1.mass)) * dotVelocity0) +
+                 ((1 + averageE) * pm_1.mass * dotVelocity1)) /
+                (pm_0.mass + pm_1.mass);
+            float finalVelocity1 =
+                (((pm_1.mass -
+                   (averageE * pm_0.mass)) * dotVelocity1) +
+                 ((1 + averageE) * pm_0.mass * dotVelocity0)) /
+                (pm_0.mass + pm_1.mass);
+
+            //Vector3 prev_pm0_Vel = initVelocity0;
+
+            //탄성력이 1 , 질량이 1 , 서로 정면으로 부딪친다고 가정할시, 
+            //대상의 속도 + 튕겨지는 내 속도 + 현재 속도 = 최종 속도 
+            //--> 정면 충돌시 대상의 속도만 남는다 
+            //[->A의 속도 --- B의 속도<-] => [<-B의 속도 --- A의 속도->] 로 속도와 방향이 바뀐다 
+            //쉽게 보면 대상의 속도(힘) 만이 적용된다고 볼 수 있다 
+            if (false == pm_0.isStatic)
+            {
+                pm_0.linearVelocity = ((finalVelocity0 - dotVelocity0) * contactNormal + initVelocity0);
+            }
+            if (false == pm_1.isStatic)
+            {
+                pm_1.linearVelocity = ((finalVelocity1 - dotVelocity1) * contactNormal + initVelocity1);
+            }
+
+
+
+
+            //---------------------------------------------------------------
+            //d1 : 1차원 , 디멘션1 
+            //DebugWide.LogBlue("#  init_vel0: " + initVelocity0 + "  init_vel1: " + initVelocity1);
+            //DebugWide.LogBlue("v0_d1 : " + dotVelocity0 + "  --  v1_d1 : " + dotVelocity1 + "   - averE: " + averageE );
+            //DebugWide.LogRed("fv0_d1 : " + finalVelocity0 + " -- fv1_d1 : " + finalVelocity1);
+            //DebugWide.LogRed("fv0_d1 - v0_d1 = " + (finalVelocity0 - dotVelocity0) + " -- fv1_d1 - v1_d1 = " + (finalVelocity1 - dotVelocity1));
+            //DebugWide.LogRed("(fv0_d1 - v0_d1) + v0_d1 = " + src.linearVelocity.magnitude + "  --  (fv1_d1 - v1_d1) + v1_d1 = " + dst.linearVelocity.magnitude);
+            //Vector3 tp = dst.location + contactNormal * dst.radius; //접점 
+            //Vector3 cr = Vector3.Cross(contactNormal, Vector3.up);
+            //DebugWide.AddDrawQ_Circle(src.location, src.radius, Color.gray);
+            //DebugWide.AddDrawQ_Circle(dst.location, dst.radius, Color.gray);
+            //DebugWide.AddDrawQ_Line(tp, tp + cr * 10, Color.gray);
+            //DebugWide.AddDrawQ_Line(tp, tp - cr * 10, Color.gray);
+            //DebugWide.AddDrawQ_Line(src.location, dst.location, Color.blue);
+            //DebugWide.AddDrawQ_Line(src.location, src.location + src.linearVelocity, Color.green);
+            //DebugWide.AddDrawQ_Line(src.location, src.location + prev_pm0_Vel, Color.green);
+            //Vector3 pm0_velpos = src.location + prev_pm0_Vel;
+            //DebugWide.AddDrawQ_Line(pm0_velpos, pm0_velpos + dotVelocity0 * contactNormal, Color.red);
+            //DebugWide.AddDrawQ_Line(pm0_velpos, pm0_velpos + finalVelocity0 * contactNormal, Color.blue);
+            //DebugWide.AddDrawQ_Line(pm0_velpos, pm0_velpos + (finalVelocity0 - dotVelocity0) * contactNormal, Color.white);
+            //---------------------------------------------------------------
+        }
+
+        public void ResolveInterpenetration(float timeInterval)
+        {
+
+            float rate_src, rate_dst;
+            float sqrVel_src = pm_0.linearVelocity.sqrMagnitude;
+            float sqrVel_dst = pm_1.linearVelocity.sqrMagnitude;
+            float sqr_sum = sqrVel_src + sqrVel_dst;
+            if (Misc.IsZero(sqr_sum)) rate_src = rate_dst = 0.5f;
+            else
+            {
+                rate_src = 1f - (sqrVel_src / sqr_sum);
+                rate_dst = 1f - rate_src;
+            }
+
+            //------------------------------
+            //한쪽만 정적일때 안밀리게 하는 예외처리 
+            if (true == pm_0.isStatic && false == pm_1.isStatic)
+            {
+                rate_src = 0;
+                rate_dst = 1f;
+            }
+            if (false == pm_0.isStatic && true == pm_1.isStatic)
+            {
+                rate_src = 1f;
+                rate_dst = 0;
+            }
+            //------------------------------
+
+
+            float len_bt_src = penetration * rate_src;
+            float len_bt_dst = penetration * rate_dst;
+
+            pm_0.location += contactNormal * len_bt_src;
+            pm_1.location += -contactNormal * len_bt_dst;
+
+        }
+
+    }
 
     [System.Serializable]
     public class Point_mass
@@ -298,7 +525,7 @@ namespace Test_ParticleBounce2
         public bool isImpluse = true; //단발성 충격힘 적용
         public bool isStatic = false; //충돌에 반응하지 않게 설정
 
-        public void Update(float changeInTime)
+        public void Intergrate(float changeInTime)
         {
         
             linearVelocity *= friction; //마찰력 간단적용 1
@@ -321,6 +548,18 @@ namespace Test_ParticleBounce2
                 forces = ConstV.v3_zero; //힘 적용후 바로 초기화 - impluse
             }
 
+        }
+
+        public void ApplyForce(Vector3 force)
+        {
+            isImpluse = false;
+            forces = force;
+        }
+
+        public void ApplyImpluse(Vector3 force)
+        {
+            isImpluse = true;
+            forces = force; 
         }
 
         public void Draw(Color color)
