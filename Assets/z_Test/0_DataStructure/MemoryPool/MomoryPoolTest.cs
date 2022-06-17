6453f0e3b65c20f4a888b91fb2d652479b2acc2f
chamto <anrudco@gmail.com>
2022-06-17 15:22:56 +0900

    [ MemoryPoolTest : 메모리풀 동작 테스트 프로젝트 추가 ]

---
+++ Assets/z_Test/0_DataStructure/MemoryPool/MomoryPoolTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UtilGS9;
+
+public class MomoryPoolTest : MonoBehaviour 
+{
+    MemoryPool<TData> _memoryPool = new MemoryPool<TData>();
+    List<TData> _datas = new List<TData>();
+
+	void Start () 
+    {
+        _memoryPool.Init(100, 20, "Test_Pool");
+
+        for(int i=0;i<20;i++)
+        {
+            TData data = _memoryPool.Alloc();
+            data.length = i;
+            _datas.Add(data);
+        }
+        DebugWide.LogBlue(_memoryPool.ToString());
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            _datas[i].Reset();
+            _memoryPool.Free(_datas[i]);
+
+        }
+        DebugWide.LogBlue(_memoryPool.ToString());
+
+        for (int i = 0; i < 100; i++)
+        {
+            TData data = _memoryPool.Alloc();
+            data.length = i;
+            _datas.Add(data);
+        }
+        DebugWide.LogBlue(_memoryPool.ToString());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class TData
+{
+    public int length = -1; 
+
+    public void Reset()
+    {
+        length = -1; 
+    }
+}
